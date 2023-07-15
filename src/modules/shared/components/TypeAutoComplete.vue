e0139bb7dcab276e044649d4819d772b391ef12b
teobeh <138968016+teobeh@users.noreply.github.com>
2023-07-15 20:53:01 -0300

    Actualizar TypeAutoComplete.vue

--- src/modules/shared/components/TypeAutoComplete.vue
+++ src/modules/shared/components/TypeAutoComplete.vue
@@ -11,7 +11,7 @@ const types = ['Mixta', 'Comparada', 'Comparada Continuada', 'Fusilada'].map((la
     :options="types"
     :value="props.value"
     :on-update:value="(value: string) => emit('update:value', value)"
-    placeholder="Ej: Western"
+    placeholder="ej: Western"
     filterable
   />
 </template>
